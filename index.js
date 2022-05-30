efa7ec136f55672be16722d273e49e0bfca11440
Alka <alkagotit@gmail.com>
2022-05-30 22:23:30 +0530

    basic rouing with pure node

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const fs=require('fs')
-fs.readFile('./txt/start.txt','utf8',(err,data)=>{
+const http=require('http');
+const url=require('url');
 
-  fs.readFile(`./txt/${data}.txt`,'utf-8',(err,data1)=>{
-    fs.writeFile('./txt/output.txt',`${data}\n ${data1}`,(err)=>{
-      console.log('file written successfully')
+const server=http.createServer((req,res)=>{
+  console.log(req.url);
+  const pathName=req.url;
+  if(pathName==='/'|| pathName==='/overview'){
+    res.end("im overview")
+  }else if(pathName==='/products'){
+    res.end("im products")
+  }else{
+    res.writeHead(404,{
+      'Content-type':'text/html'
     })
-  })
+    res.end("<h1>im not found</h1>")
 
-}
+  }
  
-
-)
-
-console.log('readinggg')
+})
+server.listen(8000,'127.0.0.1',()=>{
+  console.log('listening on port 8000')
+})
